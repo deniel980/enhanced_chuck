cbd79a472de586160e9a5cfff2e524fb2a9829a6
Daniel <deniel98045@gmail.com>
2023-02-20 17:25:44 +0100

    recent

--- next.config.js
+++ next.config.js
@@ -2,16 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       host: 'api.chucknorris.io',
-  //       port: '',
-  //       pathname: '/img/chucknorris_logo_coloured_small@2x.png',
-  //     },
-  //   ],
-  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ibb.co',
+        port: '',
+        pathname: '/xmWhkH2/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
