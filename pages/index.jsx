36ec7fbd373fd65c4ff232a57df249f90b392024
Daniel <deniel98045@gmail.com>
2023-02-21 14:37:48 +0100

    recent

--- pages/index.jsx
+++ pages/index.jsx
@@ -33,7 +33,7 @@ const Home = () => {
 
       <HeaderCustom />
       <div if="main_content_container" class="mx-auto">
-        <div id="main_content" class="md:grid-cols-2 md: w-fit mx-auto my-16  grid grid-cols-1  border-solid border-8 border-black h-auto">
+        <div id="main_content" class="md:grid-cols-2 md: w-fit mx-auto my-20  grid grid-cols-1  border-solid border-8 border-black h-auto">
 
           <div id="left_container" class="bg-[url('../public/chuck_bg.png')]">
             <button onClick={handleClick} >
@@ -41,14 +41,16 @@ const Home = () => {
                 onMouseEnter={() => setImageUrl('https://iili.io/HGlQPUu.png')}
                 onMouseLeave={() => setImageUrl('https://iili.io/HGlQ4f9.png')}
                 onClick={() => setImageUrl('https://iili.io/HGlQ6le.png')}
-                width="512" height="512" alt="Chuck Image"
+                width="512" height="300" alt="Chuck Image"
                 src={ImageUrl} />
             </button>
           </div>
 
           <div id="right_container" class=" max-w-lg max-h-25">
-            <div id="joke_container" class="border-solid border-l-4 border-black w-400px p-3 h-full font-semibold font-monospace text-xl text-black bg-white">
-              <p>{Joke}</p>
+            <div id="joke_container" class="border-solid border-l-4 border-black w-400px p-5 h-full font-semibold font-monospace text-2xl align-middle text-black bg-white">
+              
+                <p>{Joke}</p>
+              
             </div>
           </div>
 
