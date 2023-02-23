b942638a2a83dbdd7eb5a8b782f0d8c9dbfd7af4
Daniel <deniel98045@gmail.com>
2023-02-23 15:33:39 +0100

    recent

--- postgreSQL/main.sql
+++ postgreSQL/main.sql
@@ -1,9 +1,9 @@
-SELECT id, name
-FROM person
-WHERE id = 1;
-SELECT id, name
-FROM person
-WHERE id = 3;
-SELECT id, name
-FROM person
-WHERE id = 4;
+
+
+
+/*In progress*/
+
+
+/*create a database with user ip*/
+
+CREATE DATABASE "ip" WITH OWNER = postgres ENCODING = 'UTF8' TABLESPACE = pg_default LC_COLLATE = 'English_United States.1252' LC_CTYPE = 'English_United States.1252' CONNECTION LIMIT = -1;
